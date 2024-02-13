1d291412fc270daadbe74a7f4a200c0f87de2053
Ezequiel <ezequielsilvalima01@gmail.com>
2024-02-13 19:04:44 -0300

    commit

--- aula06/ex003.js
+++ aula06/ex003.js
@@ -4,6 +4,6 @@ var n2 = Number(window.prompt('Digite um número')) // O nome será guardado na 
 
 var s = n1 + n2
 
-window.alert('O resultado da soma é ' + s)
+window.alert(`A soma entre ${n1} e ${n2} é igual a : ${s}`)
 
 
