34aef5a677738a4a6c4089ce3d564033955756f2
Ezequiel <ezequielsilvalima01@gmail.com>
2024-02-13 17:36:23 -0300

    commit

---
+++ aula06/ex003.js
@@ -0,0 +1,9 @@
+var n1 = Number.parseInt(window.prompt('Digite um número')) // O nome será guardado na var nome
+
+var n2 = Number.parseInt(window.prompt('Digite um número')) // O nome será guardado na var nome
+
+var s = n1 + n2
+
+window.alert('O resultado da soma é ' + s)
+
+
