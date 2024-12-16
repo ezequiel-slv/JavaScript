b15284a7e866c5defbec9358438788b92279602c
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-16 20:57:41 -0300

    commit

---
+++ arquivos/aula05.js
@@ -0,0 +1,9 @@
+//operadores relacionais: >, >=, <, <=, ==, !=
+
+let num1 = 10
+let num2 = 30
+let num3 = 25
+
+let res = num1 <= num3 
+
+console.log(res)
---
+++ arquivos/aula06.js
@@ -0,0 +1,21 @@
+//operadores logicos: &&, ||, !
+
+let num1, num2, num3
+num1 = 5
+num2 = 8
+num3 = 5
+
+
+if((num3 > num2)|| (num3 == num1)){
+    console.log("true")
+} //true
+
+if((num1 < num2) && (num3 < num2)){
+    console.log("true")
+} //true
+
+if(!num1){
+    console.log("fslse")
+}else{
+console.log("false")//false
+}
