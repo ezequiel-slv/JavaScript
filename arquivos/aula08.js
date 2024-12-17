dda5a94e8ea811900091de8e390c2b03bc0bf533
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-17 20:24:28 -0300

    commit

---
+++ arquivos/aula08.js
@@ -0,0 +1,9 @@
+//operador ternário: ?
+
+let num1 = 10
+let num2 = 20
+
+console.log(num1 > num2 ? "verdadeiro" : "falso")
+
+//se condinção for true retorna "verdadeiro"
+//se condinção for false retorna "falso"
