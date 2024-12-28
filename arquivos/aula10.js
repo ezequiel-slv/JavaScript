12436c1fefa5042c9ae6192d627203b819b28377
Ezequiel <ezequielsilvalima01@gmail.com>
2024-12-28 20:56:03 -0300

    commit

---
+++ arquivos/aula10.js
@@ -0,0 +1,10 @@
+let velocidade = 130
+
+if((velocidade > 80) && (velocidade == 100)){
+    console.log("Aplicar multa")
+}else if(velocidade > 100){
+   console.log("Suspender a carteira")
+}else{
+    console.log("Liberar veiculo")
+}
+console.log("Fim do programa")
