c1e6b1a8b5ecff5086a9a0b51ffe8190d02b22de
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-01 14:42:54 -0300

    commit

---
+++ arquivos/whileDoWhile.js
@@ -0,0 +1,24 @@
+// while
+console.log("While")
+console.log("-------")
+
+let n=10
+let fat=1
+
+while(n>=1){
+   fat*=n
+   n--
+}
+console.log(" Do While")
+console.log("-------")
+let n2 = 10
+
+//Do while
+ do{
+    console.log(n2)
+    n2++
+   
+ }while(n2<=15)
+
+console.log("------")
+console.log("Fim do progarama")
