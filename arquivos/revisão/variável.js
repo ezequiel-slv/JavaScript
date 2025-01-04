c8d0d9964f35ee78bdcef6ed7489983c3c2a1e09
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-04 19:44:27 -0300

    commit

---
+++ arquivos/revisão/variável.js
@@ -0,0 +1,19 @@
+//faça exemplos de let, var e const
+
+let nome="Ezequiel"
+var sobrenome="Silva Lima"
+
+if(true){
+    let nome="Ezequiel"
+    console.log(nome)
+}
+
+if(true){
+    var sobrenome="Silva Lima"
+}
+
+console.log(sobrenome)
+
+
+const idade = "22"
+console.log(idade)
