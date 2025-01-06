ed81b32219ae47c2194a2cf78eaeb6e3dfac80fc
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-06 20:58:00 -0300

    commit

---
+++ arquivos/revisão/ExeOpeArit.js
@@ -0,0 +1,17 @@
+let X = parseFloat(prompt("Digite o valor de X: "))
+let Y = parseFloat(prompt("Digite o valor de Y: "))
+let Z = parseFloat(prompt("Digite o valor de Z: "))
+
+ let mult = X * Y
+ let div = Y / X
+ let adic = Z + X   
+ let sub = Y - Z        
+ let rest = Z % X
+ let expo = X**X
+ 
+ console.log("A multiplicação de X e Y é: " + mult)
+ console.log("A divisão de X e Y é: " + div)
+ console.log("A adição de X e Y é: " + adic)
+ console.log("A subtração de X e Y é: " + sub)
+ console.log("O resto de X e Y é: " + rest)
+ console.log("A exponenciação de X e Y é: " + expo)
