892b484a822857cd0bcd8f9adb00b85b0647f7d6
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-09 19:59:43 -0300

    commit

--- arquivos/revisão/operadorternario/ExeOperTern.js
+++ arquivos/revisão/operadorternario/ExeOperTern.js
@@ -1,7 +1,13 @@
 //operador ternario
-
+console.log("Teste 1")
 let num1 = 0, num2 = 0
 
 num1 = 31
 
 console.log(num1%2 == 0 ? "par" : "impar")
+
+console.log("Teste 2")
+let valor = 0
+
+valor = parseFloat(prompt("Digite o valor do teste"))
+
