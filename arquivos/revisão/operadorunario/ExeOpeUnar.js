563d47b599a4a3d2e553ee64881b861161fbfa74
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-10 18:48:30 -0300

    commit

---
+++ arquivos/revisão/operadorunario/ExeOpeUnar.js
@@ -0,0 +1,23 @@
+//faça um programa que calcule média aritimética de 3 alunos e classifique eles de acordo com a pontuação
+
+//obs: utilize o operador unário
+
+let aluno1 = "mario"
+let aluno2 = "joão"
+let aluno3 = "joana"
+
+let notamario = 8
+let notajoao = 6
+let notajoana = 7
+
+console.log(aluno1 + " tem nota " + notamario)
+console.log(aluno2 + " tem nota " + notajoao)
+console.log(aluno3 + " tem nota " + notajoana)
+
+let media = (notamario + notajoao + notajoana) / 3
+
+console.log("A média das notas dos três alunos é de: " + media)
+console.log("----------")
+console.log("Tipos de variaveis usadas: " + typeof(aluno1) + " e " + typeof(notajoana))
+
+
