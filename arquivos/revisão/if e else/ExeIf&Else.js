360a5c132351a9ea519bedefcd70d4ea828cf80b
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-11 20:33:52 -0300

    commit

---
+++ arquivos/revisão/if e else/ExeIf&Else.js
@@ -0,0 +1,34 @@
+// if e else
+
+let num1 = 3
+
+console.log("exercicio 1")
+if(num1%2==0){
+    console.log('par')
+}else{
+    console.log('impar')
+}
+console.log("-------")
+
+console.log("exercicio 2")
+let nota = 70
+
+if(nota >= 60){
+    console.log("Aprovado")
+}else {
+    console.log("Reprovado")
+}
+console.log("-------")
+
+console.log("exercicio 3")
+let A = 30
+let B = 30
+
+if(A > B){
+    console.log("A é maior que B")
+}else if(B > A){
+    console.log("B é maior que A")
+}else{
+    console.log("Os dois são iguais")
+}
+console.log("-------")
