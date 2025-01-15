70bcbc101dde8e056b0d2cd5cdb4de36330644af
Ezequiel <ezequielsilvalima01@gmail.com>
2025-01-14 23:11:40 -0300

    commit

---
+++ arquivos/revisão/loopfor/ExeLoopFor.js
@@ -0,0 +1,16 @@
+// loop for
+
+console.log("Exercício 1")
+
+for (let i = 1; i < 11; i++) {
+    console.log("iteração: " + i)
+}
+
+console.log("Exercício 2")
+
+let soma = 0
+for (let i = 1; i <= 100; i++) {
+    soma += i
+}
+
+console.log("soma de 1 a 100 acumulada: " + soma)
